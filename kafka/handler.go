5bd6559ebcee8a7a847b9c3b4208b51a31a9f52c
Parham Alvani <parham.alvani@gmail.com>
2019-03-12 14:55:22 +0330

    Prints error on duplicate messages.

--- kafka/handler.go
+++ kafka/handler.go
@@ -1,11 +1,16 @@
 package kafka
 
 import (
+	"sync"
+
 	"github.com/Shopify/sarama"
 	"github.com/sirupsen/logrus"
 )
 
+var repeatedOffsets = map[int64]bool{}
+
 type kktConsumerGroupHandler struct {
+	lck sync.Mutex
 }
 
 func (h kktConsumerGroupHandler) Setup(sess sarama.ConsumerGroupSession) error {
@@ -19,6 +24,15 @@ func (h kktConsumerGroupHandler) Cleanup(sess sarama.ConsumerGroupSession) error
 func (h kktConsumerGroupHandler) ConsumeClaim(sess sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
 	for msg := range claim.Messages() {
 		logrus.Infof("Message topic:%q partition:%d offset:%d\n", msg.Topic, msg.Partition, msg.Offset)
+
+		h.lck.Lock()
+		if repeatedOffsets[msg.Offset] {
+			logrus.Errorf("Duplicated message topic:%q partition:%d offset:%d\n", msg.Topic, msg.Partition, msg.Offset)
+		} else {
+			repeatedOffsets[msg.Offset] = true
+		}
+		h.lck.Unlock()
+
 		logrus.Debugln(string(msg.Value))
 		sess.MarkMessage(msg, "")
 	}
